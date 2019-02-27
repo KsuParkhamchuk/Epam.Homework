1ccee53cb774cf78b04b5c3f64e7b5af9d85f903
KsuParkhamchuk <43078183+KsuParkhamchuk@users.noreply.github.com>
2019-02-27 23:26:31 +0300

    Create Main.java

---
+++ 3/exercise/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Main
+{
+  public static void main(String[] args) {
+      Activity matrix=new Activity();
+      matrix.CreateMatrix();
+ 
+}
+}
